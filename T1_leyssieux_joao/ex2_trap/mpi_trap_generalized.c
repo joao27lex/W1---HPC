47d28b4425d720e3fe99908b7fa6a247be66b5b4
Linux <jplcampanella@gmail.com>
2026-06-15 16:16:30 -0300

    ADD: comentarios para fixar o entendimento da 2a

--- T1_leyssieux_joao/ex2_trap/mpi_trap_generalized.c
+++ T1_leyssieux_joao/ex2_trap/mpi_trap_generalized.c
@@ -2,18 +2,19 @@
 #include <mpi.h>
 #include <math.h>
 
+// this is the function that will be integrated. in this case, it is sin(x)
 static double f(double x) { 
     return sin(x); 
 }
 
-
+// applies the trapezoid formula to a section given interval
 static double Trap(double left, double right, int n_traps, double h) {
-    double estimate = (f(left) + f(right)) / 2.0;
+    double estimate = (f(left) + f(right)) / 2.0; // estimate starts with the average of the left and right points (y-axis)
     for (int i = 1; i <= n_traps - 1; i++) {
-        double x_i = left + i * h;
+        double x_i = left + i * h; //calculates the x value of the i-th trapezoid (x-axis)
         estimate += f(x_i);
     }
-    return estimate * h;
+    return estimate * h; //returns estimate (y-axis) multiplied by the width of the trapezoids (x-axis)
 }
 
 /* Process 0 reads a, b, n from stdin and forwards them to every other
@@ -24,16 +25,16 @@ static void Get_input(int my_rank, int comm_sz,
     if (my_rank == 0) {
         printf("Enter a, b, n: ");
         fflush(stdout);
-        scanf("%lf %lf %d", a_p, b_p, n_p);
+        scanf("%lf %lf %d", a_p, b_p, n_p); //inputs left and right points and total number of trapezoids
         for (int dest = 1; dest < comm_sz; dest++) {
-            MPI_Send(a_p, 1, MPI_DOUBLE, dest, 0, MPI_COMM_WORLD);
-            MPI_Send(b_p, 1, MPI_DOUBLE, dest, 0, MPI_COMM_WORLD);
-            MPI_Send(n_p, 1, MPI_INT,    dest, 0, MPI_COMM_WORLD);
+            MPI_Send(a_p, 1, MPI_DOUBLE, dest, 0, MPI_COMM_WORLD); //sends left point to all other processes
+            MPI_Send(b_p, 1, MPI_DOUBLE, dest, 0, MPI_COMM_WORLD); // sends right point to all other processes
+            MPI_Send(n_p, 1, MPI_INT,    dest, 0, MPI_COMM_WORLD); // sends total number of trapezoids to all other processes
         }
     } else {
-        MPI_Recv(a_p, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        MPI_Recv(b_p, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        MPI_Recv(n_p, 1, MPI_INT,    0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Recv(a_p, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE); //receives left point from process 0
+        MPI_Recv(b_p, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE); //receives right point from process 0
+        MPI_Recv(n_p, 1, MPI_INT,    0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE); // receives total number of trapezoids from process 0
     }
 }
 
@@ -47,13 +48,13 @@ int main(void) {
 
     Get_input(my_rank, comm_sz, &a, &b, &n);
 
-    h               = (b - a) / n;
-    rem             = n % comm_sz;
-    local_n         = n / comm_sz + (my_rank < rem ? 1 : 0);
-    offset          = my_rank * (n / comm_sz) + (my_rank < rem ? my_rank : rem);
-    local_a         = a + offset * h;
-    local_b         = local_a + local_n * h;
-    local_integral  = Trap(local_a, local_b, local_n, h);   
+    h               = (b - a) / n; // calculates the width of each trapezoid
+    rem             = n % comm_sz; // calculates the mod of the total number of traps by the number of processes to determine how many processes will it need to calculate an extra trap
+    local_n         = n / comm_sz + (my_rank < rem ? 1 : 0); //calculates how many traps each process will calculate
+    offset          = my_rank * (n / comm_sz) + (my_rank < rem ? my_rank : rem); //calculates where each process will start calculating the traps
+    local_a         = a + offset * h; //calculates the left point of the interval that each process will calculate
+    local_b         = local_a + local_n * h; //calculates the right point of the interval that each process will calculate
+    local_integral  = Trap(local_a, local_b, local_n, h); //calculates the integral of the local interval usign trap function
 
     if (my_rank != 0) {
         MPI_Send(&local_integral, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);

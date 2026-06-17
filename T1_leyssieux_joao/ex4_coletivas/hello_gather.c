562e85fcdfdc62def479177b1927285d1f760c69
Linux <jplcampanella@gmail.com>
2026-06-17 15:20:57 -0300

    ADD: ex-4-a & ex-4-b

---
+++ T1_leyssieux_joao/ex4_coletivas/hello_gather.c
@@ -0,0 +1,48 @@
+#define _GNU_SOURCE
+
+#include <unistd.h>   // getpid()
+#include <sched.h>    // sched_getcpu()
+#include <mpi.h>
+
+//default libs
+#include <stdio.h>
+#include <string.h>
+
+
+#define MAX_STRING 100
+
+int main(int argc, char* argv[]){
+    MPI_Init(&argc, &argv);
+
+    int comm_size, my_rank, my_cpu;
+    pid_t my_pid; //proccess identifier
+    char greeting[MAX_STRING]; //size of the greeting string
+
+    MPI_Comm_size(MPI_COMM_WORLD, &comm_size); //how many processes?
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank); //which one am I?
+
+    my_pid = getpid(); //gets the process identifier
+    my_cpu = sched_getcpu();
+
+    const char *message = "Hello from rank %d/%d -- PID = %d, CPU = %d"; //stores the pointer to the message format
+
+    snprintf(greeting, MAX_STRING, message, my_rank, comm_size, my_pid, my_cpu); //stores the message in 'greeting' 
+
+    char *gather_greetings = NULL; //pointer to store the greetings gathered by process 0
+    if(my_rank == 0){
+        gather_greetings = (char*)malloc(comm_size * MAX_STRING * sizeof(char)); //allocates memory to store the greetings of all processes in process 0 
+    }
+
+    MPI_Gather(greeting, MAX_STRING, MPI_CHAR, gather_greetings, MAX_STRING, MPI_CHAR, 0, MPI_COMM_WORLD);  
+
+    if(my_rank == 0){
+        for(int i = 0; i < comm_size; i++){
+            printf("%s\n", &gather_greetings[i * MAX_STRING]); //prints the greeting of each process by acessing the string
+        }
+    }
+
+    free(gather_greetings); //clears the memory allocated for the gathered greetings
+
+    MPI_Finalize();
+    return 0;
+}
---
+++ T1_leyssieux_joao/ex4_coletivas/minmax.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h> 
+#include <time.h>
+#include <mpi.h>
+#include <math.h>
+
+int main(int argc, char** argv){
+    int my_rank, comm_sz, splitted_block_size, rem_block_size;
+    double local_max, local_min, global_max, global_min;
+    double *randomRootVector = NULL; 
+    double *splittedVector = NULL;
+    double serial_min = 0.0;
+    double serial_max = 0.0;
+
+    int n = 1000;
+    
+    MPI_Init(&argc, &argv);
+    
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank); 
+    MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
+    
+    block_size = n / comm_sz;
+    rem_block_size = n % comm_sz;
+    
+    randomRootVector = (double*)malloc(n * sizeof(double)); //allocates memory for the random doubles in process 0
+    splittedVector = (double*)malloc(block_size * sizeof(double)); //allocates memory for the splitted vectors in each process
+
+    if(my_rank == 0){
+        srand(time(NULL));
+        for (int i = 0; i < n; i++){
+            randomRootVector[i] = (double)rand() / RAND_MAX; //generates a random double and stores it in the i position of the vector
+            if(randomRootVector[i] < serial_min){
+                serial_min = randomRootVector[i]; //updates the serial_min if it's smaller than the current value
+            }
+            if(randomRootVector[i] > serial_max){
+                serial_max = randomRootVector[i]; //updates the serial_max if it's bigger than the current value
+            }
+        }
+    }
+
+    MPI_Scatter(randomRootVector, block_size, MPI_DOUBLE, splittedVector, block_size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+
+//------------------------------------------------------------------------------------------------------------------------------------
+
+    local_min = splittedVector[0]; //starts with the first value
+    local_max = splittedVector[0]; //starts with the first value
+
+    for(int i = 1; i < block_size; i++){
+        if(splittedVector[i] < local_min){
+            local_min = splittedVector[i];
+        }
+        if(splittedVector[i] > local_max){
+            local_max = splittedVector[i];
+        }
+    }
+
+    MPI_Reduce(&local_min, &global_min, 1, MPI_DOUBLE, MPI_MIN, 0, MPI_COMM_WORLD);  //global_min -> local_min
+    MPI_Reduce(&local_max, &global_max, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD); //global_max -> local_max
+
+//--------------------------------------------------------------------------------------------------------------------------------------------
+
+    if(my_rank == 0){
+        printf("Serial min: %f\n", serial_min);
+        printf("Serial max: %f\n", serial_max);
+        printf("Global min: %f\n", global_min);
+        printf("Global max: %f\n", global_max);
+    }
+
+    free(randomRootVector);
+    free(splittedVector);
+
+    MPI_Finalize();
+    return 0;   
+}
